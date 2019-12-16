24f9adeca3dcd7005881c2bff0e3794255990875
Vanja Veselinovic <vanja.veselinovic.95@gmail.com>
2019-12-15 23:06:57 -0500

    update

--- script.js
+++ script.js
@@ -1,5 +1,73 @@
 $(document).ready(() => {
-	$('.card').on('click', (e) => {
-		console.log($(e.target).slideUp(200));
+	let dragging = false;
+	let currentCardElement;
+	const startCardPosition = {
+		x: 0,
+		y: 0,
+	};
+	const startTouchPosition = {
+		x: 0,
+		y: 0,
+	};
+	const prevTouchPosition = {
+		x: 0,
+		y: 0,
+	};
+	const currTouchPosition = {
+		x: 0,
+		y: 0,
+	};
+
+	const cardElements = $('.card-container');
+	let index = cardElements.length - 1;
+
+	$(cardElements[index]).addClass('current');
+
+	$('.card-container').on('touchstart', (e) => {
+		currentCardElement = e.delegateTarget;
+		$(currentCardElement).addClass('dragging');
+
+		startCardPosition.x = currentCardElement.offsetLeft;
+		startCardPosition.y = currentCardElement.offsetTop;
+
+		startTouchPosition.x = e.touches[0].clientX;
+		startTouchPosition.y = e.touches[0].clientY;
+		
+		dragging = true;
+	});
+
+	$('.card-container').on('touchmove', (e) => {
+		if (dragging && currentCardElement) {
+			prevTouchPosition.x = currTouchPosition.x;
+			prevTouchPosition.y = currTouchPosition.y;
+
+			currTouchPosition.x = e.touches[0].clientX;
+			currTouchPosition.y = e.touches[0].clientY;
+
+			currentCardElement.style.left = (startCardPosition.x + currTouchPosition.x - startTouchPosition.x) + 'px';
+			currentCardElement.style.top = (startCardPosition.y + currTouchPosition.y - startTouchPosition.y) + 'px';
+		}
+	})
+
+	$('.card-container').on('touchend', () => {
+		if (Math.abs(currTouchPosition.x - prevTouchPosition.x) > 5 || Math.abs(currTouchPosition.y - prevTouchPosition.y) > 5) {
+			$(currentCardElement).animate({
+				left: currentCardElement.offsetLeft + (window.innerWidth * (currTouchPosition.x - prevTouchPosition.x)),
+				top: currentCardElement.offsetTop + (window.innerHeight * (currTouchPosition.y - prevTouchPosition.y)),
+			}, 500, () => {
+				$(currentCardElement).addClass('done');
+			});
+			$(currentCardElement).removeClass('current');
+			index--;
+			$(cardElements[index]).addClass('current');
+		} else {
+			$(currentCardElement).animate({
+				top: 0,
+				left: 0,
+			}, 100, () => {
+				$(currentCardElement).removeClass('dragging');
+			});
+		}
+		dragging = false;
 	});
 });
